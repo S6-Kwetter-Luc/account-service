76bd438d80486d0d298222995714d4d6d9297c8c
Luc Urlings <urlingsluc@gmail.com>
2020-05-27 10:37:39 +0200

    feature/KL-97: Added Email Service Message queuesupport. Updated tests

---
+++ account-service/MQSettings/MessageQueueSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace account_service.MQSettings
+{
+    public class MessageQueueSettings
+    {
+        public string Uri { get; set; }
+        public string Exchange { get; set; }
+    }
+}
